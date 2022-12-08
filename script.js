f0bf91932901724b3d9a0440b015e4f84551aacf
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-12-08 23:54:44 +0500

    menu bar work done

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";    
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
