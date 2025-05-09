938e7e52ed80023485d5a6dd43a68f0816ae7ba5
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2025-05-10 01:29:37 +0500

    Scroll Progress Bar
    Scroll Progress Bar added

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progres-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((post * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+}
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
